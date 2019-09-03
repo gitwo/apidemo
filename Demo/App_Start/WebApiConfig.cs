09a17a95cf3b2d6b43cce26e50d802b1993dfbd6
vip <vip@outlook.com>
2019-09-03 14:47:42 +0800

    1. ThrottlePolicy 的另一种使用方法

--- Demo/App_Start/WebApiConfig.cs
+++ Demo/App_Start/WebApiConfig.cs
@@ -36,6 +36,7 @@ namespace Demo
 			{
 				DateTimeFormat = "yyyy-MM-dd HH:mm:ss"
 			});
+
 			//json序列化null数据忽略
 			format.JsonFormatter.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
 
@@ -49,10 +50,33 @@ namespace Demo
 			//api限流实现
 			config.MessageHandlers.Add(new ThrottlingHandler()
 			{
-				Policy = ThrottlePolicy.FromStore(new PolicyConfigurationProvider()),
+				Policy = ThrottlePolicy.FromStore(new PolicyConfigurationProvider()),//在config中定义限制策略
 				Repository = new ThrottleRepository()
 			});
 
+			/*
+			 * 直接配置形式
+			new ThrottlePolicy(perSecond: 1, perMinute: 20, perHour: 200)
+			{
+				IpThrottling = true,
+				IpRules = new System.Collections.Generic.Dictionary<string, RateLimits> {
+					{ "192.168.1.1", new RateLimits { PerSecond = 2 } },
+					{ "192.168.2.0/24", new RateLimits { PerMinute = 30, PerHour = 30*60, PerDay = 30*60*24 } }
+				},
+				ClientThrottling = true,
+				ClientRules = new System.Collections.Generic.Dictionary<string, RateLimits>
+				{
+					 { "api-client-key-1", new RateLimits { PerMinute = 40, PerHour = 400 } },
+					 { "api-client-key-9", new RateLimits { PerDay = 2000 } }
+				},
+				EndpointThrottling = true,
+				EndpointRules = new System.Collections.Generic.Dictionary<string, RateLimits>
+				{
+					 { "api/search", new RateLimits { PerSecond = 10, PerMinute = 100, PerHour = 1000 } }
+				},
+				StackBlockedRequests = true,//把被拒绝的请求也计算到其他的计数器里(分钟、小时、天)
+			};
+			*/
 		}
 	}
 }
